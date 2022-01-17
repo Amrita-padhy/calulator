d0035732e945d39bfada677ff983c671f4c50e0e
amrita-padhy <amrita.padhy5@gmail.com>
2022-01-17 16:17:32 +0530

    end commit

--- calculator.js
+++ calculator.js
@@ -2,8 +2,9 @@ const inputItem = document.querySelector("#equation")
 const outputItem = document.querySelector(".results")
 const form = document.querySelector("#equation_form")
 // console.log(outputItem);
-
+const PATANTHESIS_REGEX = /\((?<equation>[^\(\)]*)\)/
 const MULTIPLY_DIVIDE_REGEX = /(?<operant_1>\d+)\s*(?<operation>[\/\*])\s*(?<operant_2>\d+)/
+const EXPONET_REGEX = /(?<operant_1>\d+)\s*(?<operation>\^)\s*(?<operant_2>\d+)/
 const ADD_SUBTRACT_REGEX = /(?<operant_1>\d+)\s*(?<operation>[\+\-])\s*(?<operant_2>\d+)/
 
 form.addEventListener("submit", e => {
@@ -13,18 +14,36 @@ form.addEventListener("submit", e => {
 })
 
 function parse(equation) {
-    if (equation.match(MULTIPLY_DIVIDE_REGEX)) {
+    console.log((equation.match(PATANTHESIS_REGEX)));
+    if (equation.match(PATANTHESIS_REGEX)) {
+        console.log(equation.match(PATANTHESIS_REGEX).groups.equation);
+        const subEquation = equation.match(PATANTHESIS_REGEX).groups.equation
+        console.log(subEquation);
+        const result = parse(subEquation)
+
+        const newEquation = equation.replace(PATANTHESIS_REGEX,result)
+        return parse(newEquation)
+
+    }
+    else if (equation.match(EXPONET_REGEX)) {
+        const result = handleMath(equation.match(EXPONET_REGEX).groups)
+        const newEquation = equation.replace(EXPONET_REGEX, result)
+        return parse(newEquation)
+
+    } 
+    else if (equation.match(MULTIPLY_DIVIDE_REGEX)) {
         const result = handleMath(equation.match(MULTIPLY_DIVIDE_REGEX).groups)
         const newEquation = equation.replace(MULTIPLY_DIVIDE_REGEX, result)
         return parse(newEquation)
 
-    } else if (equation.match(ADD_SUBTRACT_REGEX)) {
+    } 
+    else if (equation.match(ADD_SUBTRACT_REGEX)) {
         const result = handleMath(equation.match(ADD_SUBTRACT_REGEX).groups)
         const newEquation = equation.replace(ADD_SUBTRACT_REGEX, result)
         return parse(newEquation)
     }
     else {
-        return equation
+        return parseFloat(equation)
     }
 
 
@@ -42,5 +61,7 @@ function handleMath({ operant_1, operant_2, operation }) {
             return number1 + number2
         case "-":
             return number1 - number2
+            case "^":
+            return number1 ** number2
     }
 }
